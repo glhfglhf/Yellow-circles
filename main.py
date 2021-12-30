02ef9f981a4ea627564f80e140821f877615a3d0
glhfglhf <kuchukov_marat@mail.ru>
2021-12-30 22:58:27 +0300

    Number 3

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import sys
 from random import randint
 
-from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 
 
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setGeometry(200, 200, 800, 600)
+
         self.draw = False
         self.setMouseTracking(True)
-        uic.loadUi('UI.ui', self)
-        self.pushButton.clicked.connect(self.run)
+
+        self.btn = QPushButton('Кнопка', self)
+        self.btn.resize(800, 80)
+        self.btn.move(0, 520)
+        self.btn.clicked.connect(self.run)
 
     def paintEvent(self, event):
         # Создаем объект QPainter для рисования
@@ -27,7 +31,7 @@ class Example(QMainWindow):
 
     def draw_someth(self, qp):
         # Задаем кисть
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         # Рисуем прямоугольник заданной кистью
         size = randint(20, 250)
         qp.drawEllipse(self.x - size // 2, self.y - size // 2, size, size)
